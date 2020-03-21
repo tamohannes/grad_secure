e902e1ba42d202ba619c0917c293bb897fa0af05
HovhannesTamoyan <hovhannes.tamoyan@gmail.com>
2020-03-21 18:31:09 +0000

    commit

--- main.py
+++ main.py
@@ -185,3 +185,16 @@ if __name__ == '__main__':
 # 	CustomLog ${APACHE_LOG_DIR}/access.log combined
 
 # </VirtualHost>
+
+
+
+
+# a2enmod proxy
+# a2enmod proxy_http
+# a2enmod proxy_ajp
+# a2enmod rewrite
+# a2enmod deflate
+# a2enmod headers
+# a2enmod proxy_balancer
+# a2enmod proxy_connect
+# a2enmod proxy_html
